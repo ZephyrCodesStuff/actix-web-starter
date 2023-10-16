0f2c297d55327c84fd64cbbe5de31af1c2261016
ZephyrCodesStuff <zephyrpersonal15@gmail.com>
2023-10-16 17:47:12 +0200

    Added logger middleware and print

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, Responder, middleware::Logger};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -14,9 +14,12 @@ async fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT").unwrap_or(String::from("8080"))
         .parse::<u16>().expect("PORT must be a number");
 
+    log::info!("Starting server at {}:{}", host, port);
+
     HttpServer::new(|| 
         App::new()
             .service(index)
+            .wrap(Logger::default())
     )
         .bind((host, port))?
         .run()
